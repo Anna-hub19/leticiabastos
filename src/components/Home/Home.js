faf0d355cb3a702106be9ad001c0faf530939d21
Anna Beatriz <annabastosbeatriz@gmail.com>
2025-01-06 17:38:59 -0300

    update img logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,12 +15,12 @@ function Home(){
             <div className='container-home-titulo'>
                 <h1 className='nome-pc'>Letícia Bastos Oliveira</h1>
                 <h2 className='nome-pc'>Psicóloga infantil <br/>CRP 19/4835 </h2>
-                <h4>  Especializada em Análise do Comportamento <br/> Aplicada (ABA) ao Autismo.</h4>
+                <h4>  Especialista em Análise do Comportamento <br/> Aplicada (ABA) ao Autismo.</h4>
                 <h1>Contato</h1>
                 <div className='div-links-home'>
-                    <a className='links-home-contato' href='https://instagram.com/psi.leticiaba' target="_blank"> <img src="./imagens/logoInst.png" className='img-logo-links'/></a>
-                    <a className='links-home-contato' href='mailto:psi.leticiabastos@gmail.com' target="_blank"> <img src="./imagens/logoG.png" className='img-logo-links'/></a>
-                    <a className='links-home-contato' href='https://wa.me/5579996616342' target="_blank"> <img src="./imagens/logoW.png" className='img-logo-links'/></a>
+                    <a className='links-home-contato' href='https://instagram.com/psi.leticiaba' target="_blank"> <img src="./imagens/LogoInstagram.png" className='img-logo-links'/></a>
+                    <a className='links-home-contato' href='mailto:psi.leticiabastos@gmail.com' target="_blank"> <img src="./imagens/LogoGmail.png" className='img-logo-links'/></a>
+                    <a className='links-home-contato' href='https://wa.me/5579996616342' target="_blank"> <img src="./imagens/LogoWhatsApp.png" className='img-logo-links'/></a>
                 </div>
             </div>
            
